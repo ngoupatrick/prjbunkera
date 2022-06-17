61a9495a6d90b2f8a8715c0a4580840ec2f6d051
Patrick <ngounepatrick@gmail.com>
2022-06-17 14:26:56 +0000

    adding config for streamlit

--- app.py
+++ app.py
@@ -1,8 +1,43 @@
 import streamlit as st  # type:ignore
 import requests
 
-baseREMOTEURL = "https://montegure.deta.dev/basic"
-baseLocalURL = "http://192.168.5.246:5000/basic"
-st.write("test")
-data = requests.get(baseREMOTEURL+"/users").json()
-st.write(data)
+def hide_menu(st):
+    
+    hide_streamlit_style = """
+            <style>
+            MainMenu {visibility: hidden;}    
+            header {visibility: hidden;}
+            footer {visibility: hidden;}
+            
+            footer:after {
+                content:'Projet OMER'; 
+                visibility: visible;
+                display: block;
+                position: relative;
+                #background-color: gray;
+                padding: 5px;
+                top: 2px;
+            }       
+               
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    
+def main():
+    st.set_page_config(
+        page_title="Nanpson REST client test",
+        page_icon="🧊",
+        initial_sidebar_state="collapsed"
+    )
+    #hide footer and header
+    hide_menu(st=st)
+    
+    baseREMOTEURL = "https://montegure.deta.dev/basic"
+    baseLocalURL = "http://192.168.5.246:5000/basic"
+    st.write("test")
+    data = requests.get(baseREMOTEURL+"/users").json()
+    st.write(data)
+
+if __name__ == '__main__':
+    main()
+
